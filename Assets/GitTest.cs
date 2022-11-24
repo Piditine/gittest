688007863e61edc139dab27afa6b2c306e9b38a7
Piditine <Piditine@ljh.com>
2022-11-24 19:13:04 +0800

    第一次提交

---
+++ Assets/GitTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    public string test01 = "这是一个测试";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
